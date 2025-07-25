02150907744022edae04858ce7ffe128fb9edffd
NirajsinghCode1008 <nirajsinghns1008@gmail.com>
2025-07-25 15:48:54 +0530

    Fix indentation and formatting for list_num operations in list_task.py

--- list_task.py
+++ list_task.py
@@ -13,3 +13,10 @@ numbers.remove(30)
 print("after removing 30 from the list",numbers)
 
  
+#task3
+list_num=[1,2,3,4,5,6,7]
+print("the first three number is",list_num[0:3])
+#print last 2 number
+print("the last 2 number from the list",list_num[5:])
+#every second element in the list
+print("the last 2 number from the list",list_num[::2])
