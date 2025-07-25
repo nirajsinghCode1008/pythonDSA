f6ce59b8b9afbbfa212890afa7ae808759fd6e19
NirajsinghCode1008 <nirajsinghns1008@gmail.com>
2025-07-25 13:31:52 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+'''Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+
+
+num = int(input()) # taking user input
+
+#printing to the console 
+print(num)
+'''
+num = [5,6,7,7]
+num.remove(7)
+print(num)
