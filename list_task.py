f8a5e1d54eaeebb99417064242d5747400362e2b
NirajsinghCode1008 <nirajsinghns1008@gmail.com>
2025-07-25 15:27:24 +0530

    Add list operations to list_task.py for appending and removing elements

--- list_task.py
+++ list_task.py
@@ -2,4 +2,14 @@
 #list of five fruits
 five_fruits=["apple","banana","carrot","mango","cherry"]
 print(five_fruits[2])
+#task2
+#list operation 
+numbers=[10,20,30,40,50]
+#adding 60 in the ens of the list
+numbers.append(60)
+print(numbers)
+#and remove 30 from the list
+numbers.remove(30)
+print("after removing 30 from the list",numbers)
+
  
