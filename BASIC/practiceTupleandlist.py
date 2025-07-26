0ab38793451325565ded51e3a27ff6921e7ae0dd
NirajsinghCode1008 <nirajsinghns1008@gmail.com>
2025-07-26 20:43:40 +0530

    Update practiceTupleandlist.py to prompt for three favorite movies and store them in a list

--- BASIC/practiceTupleandlist.py
+++ BASIC/practiceTupleandlist.py
@@ -1,1 +1,9 @@
-#
+"""Wap to askthe user to enter name of their 3 favourite movies and store them in alist."""
+favourite=[]
+favourite1=input(str("enter your first favourite movie: "))
+favourite2=input(str("enter your  second movie: "))
+favourite3=input(str("enter your third favourite movie: "))
+favourite.append(favourite1)
+favourite.append(favourite2)
+favourite.append(favourite3)
+print(favourite)
