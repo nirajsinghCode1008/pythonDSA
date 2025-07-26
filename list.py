594ebf31556a7feeecb5b6debdab46bc04ea8667
NirajsinghCode1008 <nirajsinghns1008@gmail.com>
2025-07-26 20:01:33 +0530

    Refactor list.py to comment out unused code and add new list operations

--- list.py
+++ list.py
@@ -1,8 +1,15 @@
-my_list = ["apple" , "ball" , 1]
+#y_list = ["apple" , "ball" , 1]
 
-print(my_list) #
+#print(my_list) #
 
 #adding an item to the list
-my_list.append("orange")
+#my_list.append("orange")
 
-print(my_list)  # Output: ['apple', 'ball', 1, 'orange']
+#print(my_list)  # Output: ['apple', 'ball', 1, 'orange']
+list1 = ['a', 'b' , 'c']
+list2 = [1, 2, 3]
+n = len(list1)
+for x in range (n): 
+    if x == 1:
+        print(list1[1])  
+  
