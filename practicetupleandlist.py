c4b9f681abb586411ddd412e00085e2a73c0867e
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-07-29 21:06:14 +0530

    Refactor practicetupleandlist.py to remove movie input code and enhance palindrome check functionality

--- practicetupleandlist.py
+++ practicetupleandlist.py
@@ -1,10 +1,22 @@
-#write a program to ask the user to enter a name of teir favourite movie and store in list.
-movie=[]
-favourite1=input("Enter the name of your 1st favourite movie: ")
-favourite2=input("Enter the name of your 2nd favourite movie: ")
-favourite3=input("Enter the name of your 3rd favourite movie: ")
-movie.append(favourite3)
-movie.append(favourite2)
-movie.append(favourite1)
-print(movie)
+#write a program to check if a list contain a palindromeof elements .hint(use copy method())
+element=["apple","ball","appe"]
+element1=element.copy()
+element1.reverse()
+if(element==element1):
+    print("yes it is palindrone")
+else:
+    print("no it is not palindrone")    
+
+#taking a input from user  three inpura,b,c crate alist and append into that list
+variable=input("enter 1st letter: ")
+variable1=input("enter 2nd letter: ")
+variable2=input("enter 3rd letter: ")
+variable3=variable+variable1+variable2
+variable4=list(variable3)
+variable4.append("a")
+variable4.append("b")
+variable4.append("c")
+print(variable4)
+
+
 
