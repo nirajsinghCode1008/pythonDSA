1f09e2e0649956dd331f1f453e4382be351b5c34
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-08-14 15:18:58 +0530

    Add even number summation function and pattern printing script

---
+++ evenNum.py
@@ -0,0 +1,16 @@
+def evenNum(num):
+    sum =0
+    for i in range(num):
+        if(i % 2 == 0):
+            sum+=i
+    return sum
+
+    
+
+# You can change this value to test with different numbers
+num = 10
+
+print(evenNum(num))
+# 2 +4+6+8
+
+
