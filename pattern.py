4d75b965c5469b150e21b913ea222b5981ab444a
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-08-21 14:44:02 +0530

    Refactor pattern printing code; clean up comments and improve readability

--- pattern.py
+++ pattern.py
@@ -1,19 +1,13 @@
-'''num=5
-for i in range(num):
-    for j in range (num):
-        print("*",end=" ")
-    print()'''
+"""#print the pattern like
+*****
+****
+***
+**
+*
+"""
+num=5
 
-#increasing traingle pattern
-'''n=5
-for i in range(n):
-    for j in range (i+1):#we have to increase the range of column to print by 1 
-        print("*",end=' ')
+for i in range(num):
+    for j in range(num-i):
+        print("*",end=" ")
     print()
-'''
-#pattern 3 Decreasing pattern
-n=5
-for i in range(n):# no of rows
-    for j in range (i , n):#we have to increase the range of column to print by 1 
-        print("*",end=' ')
-    print()#printing
