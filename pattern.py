edfa1f5a8b8e6865a6196cda879bc4fa82ddf9f5
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-08-23 09:15:18 +0530

    Add multiple pattern printing functions; enhance code organization and readability

--- pattern.py
+++ pattern.py
@@ -11,3 +11,87 @@ for i in range(num):
     for j in range(num-i):
         print("*",end=" ")
     print()
+
+
+
+"""write a program to print a pattern
+*****
+*****   
+*****   
+*****   
+*****   
+*****   
+"""
+num=5
+
+for i in range(num):
+    for j in range(num):
+        print(" *",end=" ")
+    print()
+
+
+"""print a pattern like    
+*
+**
+***
+****
+*****"""
+num=5
+
+for i in range(num):
+    for j in range(i+1):
+        print(" *",end=" ")
+    print()    
+#num+=1
+
+
+"""write a program to print a pattern like
+     *
+    **
+   ***  
+  ****
+ ***** """
+num=5
+for i in range(num):
+    for j in range(i,num):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")    
+    print()    
+#num+=1
+
+
+"""
+write a program to print a pattern like
+ *****
+  ****
+   ***
+    **
+     * """ 
+num=5
+for i in range(num):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(num-i):
+        print("*",end=" ")    
+    print()    
+#num+=1
+
+
+
+"""#write program to print the star like this
+ **********
+  ********
+   ******
+    ****
+     **"""
+num=5
+for i in range(num):
+    for j in range(i):
+        print(" ",end=" ")
+    for j in range(num-i ):
+        print("*",end=" ") 
+    for j in range(num-i ):
+        print("*",end=" ")       
+    print()    
+    
