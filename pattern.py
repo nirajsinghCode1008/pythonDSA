ab8ca27a0e935998a1162d13676ce1d6f5e2f155
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-08-24 17:42:59 +0530

    can you check

--- pattern.py
+++ pattern.py
@@ -148,4 +148,20 @@ for i in range(1,n+1):
             for j in range(i):
                 print(j+1,end=" ")
                
-            print()    
+            print()
+
+
+""""  
+write a program to print a pattern like this
+1
+22
+333
+4444
+55555
+"""
+n=5
+for i in range(1,n+1):
+            for j in range(i):
+                print(i,end=" ")
+               
+            print()
