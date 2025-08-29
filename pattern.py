965de8db07254934a76942e34001e4eb227456f8
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-08-29 11:54:15 +0530

    Add functions to print diamond and inverted triangle patterns

--- pattern.py
+++ pattern.py
@@ -181,3 +181,46 @@ for i in range(0,n+1):
     for j in range(n-i):
         print(j+1,end=" ")
     print()
+
+
+
+    """
+    Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+    *
+   ***
+  *****
+ *******
+*********
+Print the pattern in the function given to you.
+    """
+class Solution:
+    def pattern7(self, n):
+     for i in range(0,n):
+        for j in range(n-i-1):
+            print(" ",end="")
+        for j in range(i+1):
+            print("*",end="")
+        for j in range(i):
+            print("*",end="")    
+        print()
+
+
+
+
+""" 
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+*********
+ *******
+  *****
+   ***
+    *
+Print the pattern in the function given to you.          
+""" 
+class Solution:
+    def pattern8(self, n):
+        for i in range(n):
+            for j in range(i):
+                print(" ",end="")
+            for j in range((n-i)+(n-i)-1):
+                print("*",end="")
+            print()           
