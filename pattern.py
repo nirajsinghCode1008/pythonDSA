35de5d7c78acc4b9901996ed0e2c15e51e6a34ca
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-08-29 18:41:39 +0530

    Add function to print a sequential number pattern based on integer input

--- pattern.py
+++ pattern.py
@@ -339,3 +339,27 @@ class Solution:
                  print(i-j,end="")
             print()    
 
+
+"""
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+
+
+1 
+
+2 3 
+
+4 5 6 
+
+7 8 9 10 
+
+11 12 13 14 15
+Print the pattern in the function given to you."""
+class Solution:
+    def pattern13(self, n):
+        count =1
+        for i in range(1,n+1):
+            for j in range(i):
+                print(count,end=" ")
+                count+=1
+            print()
