5dfe61155d3ef607960c893f2890684c4f81d6ec
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-08-29 12:07:08 +0530

    Add function to print a specific star pattern with decreasing and increasing rows

--- pattern.py
+++ pattern.py
@@ -262,3 +262,31 @@ def pattern9(self, n):
             print()
        
 
+"""
+iven an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+*
+
+**
+
+***
+
+****
+
+*****
+
+****
+
+***
+
+**
+
+*
+Print the pattern in the function given to you.
+"""
+class Solution:
+    def pattern10(self, n):
+        for i in range(1,n+1):
+            print((i)*"*")
+        for i in range(n-1):
+            print((n-i-1)*"*")
+            
