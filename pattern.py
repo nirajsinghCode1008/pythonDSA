b29abda06410b91505cfb3ccf1993bb0a35f476a
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-08-29 12:10:00 +0530

    Add function to print a binary pattern based on integer input

--- pattern.py
+++ pattern.py
@@ -290,3 +290,27 @@ class Solution:
         for i in range(n-1):
             print((n-i-1)*"*")
             
+"""
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+
+
+1 
+
+0 1 
+
+1 0 1 
+
+0 1 0 1 
+
+1 0 1 0 1
+Print the pattern in the function given to you."""
+class Solution:
+    def pattern11(self, n):
+        for i in range(1,n+1):
+            for j in range(1,i+1):
+                if (i+j)%2==0:
+                    print(1,end=" ")
+                else:
+                    print(0,end=" ")
+            print()        
