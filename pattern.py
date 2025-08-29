fb88908adb1eced48f81df3515520bedf290d71f
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-08-29 13:25:15 +0530

    Add function to print a specific number pattern based on integer input

--- pattern.py
+++ pattern.py
@@ -314,3 +314,28 @@ class Solution:
                 else:
                     print(0,end=" ")
             print()        
+
+
+
+"""
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+
+
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+Print the pattern in the function given to you."""
+class Solution:
+    def pattern12(self, n):
+        for i in range(1,n+1):
+            for j in range(i):
+                print(j+1,end="")
+            for j in range(n-i): 
+                print("  ",end="")
+            for j in range(i):
+                 print(i-j,end="")
+            print()    
+
