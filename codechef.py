c3b8eca17040ebb4ae52c5c4443c1814a7896f82
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-09-05 11:45:15 +0530

    Add functionality to calculate profit from chocolate sales based on daily goals

--- codechef.py
+++ codechef.py
@@ -31,6 +31,41 @@ while t > 0:
         print("NO")
 
 
+"""
+Candy Store
+Chef has started working at the candy store. The store has 
+100
+100 chocolates in total.
+
+Chef’s daily goal is to sell 
+X
+X chocolates. For each chocolate sold, he will get 
+1
+1 rupee. However, if Chef exceeds his daily goal, he gets 
+2
+2 rupees per chocolate for each extra chocolate.
+
+If Chef sells 
+Y
+Y chocolates in a day, find the total amount he made.
+"""
+t = int(input())
+
+while t > 0:
+    x, y = map(int, input().split())
+    # Your code goes here
+    t -= 1
+    
+    extra_choc=y-x
+    profit_extra_choc=extra_choc*2
+    
+    if y<x:
+        print(y)
+    else:
+        print(x+profit_extra_choc)
+    
+    
+
     
 
 
