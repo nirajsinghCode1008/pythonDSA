1d4e472d043a04d7098cc677dc587ce57560dfe6
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-09-05 19:18:20 +0530

    Remove unnecessary blank line in the main loop

--- codechef.py
+++ codechef.py
@@ -103,6 +103,15 @@ for _ in range(n):
     nums.sort()
     print(nums[1])
    
+   """
+   Chef wants to become fit for which he decided to walk to the office and return home by walking. It is known that Chef's office is 
+X
+X km away from his home.
+
+If his office is open on 
+5
+5 days in a week, find the number of kilometers Chef travels through office trips in a week.
+   """
     
     
     
