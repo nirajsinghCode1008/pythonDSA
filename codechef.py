e5470d2e5fcf02eee7df832f7449bb8477126b17
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-09-05 11:21:23 +0530

    Add functionality to determine if a triangle is scalene based on user input

--- codechef.py
+++ codechef.py
@@ -13,4 +13,25 @@ for _ in range(t):
         n=n//10
     print(count)
 
+    """Scalene Triangle
+Given 
+A,B and
+C as the sides of a triangle, find whether the triangle is scalene.
+    """
+    t = int(input())
+
+while t > 0:
+    a, b, c = map(int, input().split())
+    # Your code goes here
+    t -= 1
+    if a!=b!=c:
+        print("YES")
+        
+    else:
+        print("NO")
+
+
+    
+
+
     
