aa885ce44a3ac0da2f7151a37e215505e7d8956a
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-09-17 21:14:41 +0530

    number of divisor in list format

--- codechef.py
+++ codechef.py
@@ -233,7 +233,22 @@ class Solution:
         
         return GCD                        
 
-            
-                 
+"""You are given an integer n. You need to find all the divisors of n. Return all the divisors of n as an array or list in a sorted order.
+
+A number which completely divides another number is called it's divisor.
+
+
+Examples:
+Input: n = 6
+Output = [1, 2, 3, 6]
+Explanation: The divisors of 6 are 1, 2, 3, 6.
+"""            
+class Solution:
+    def divisors(self, n):
+        null=[]
+        for i in range(1,n+1):
+            if n%i==0:
+                null.append(i)
+        return(null)
         
         
