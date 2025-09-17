0ca7216ca6fd23a20339aae1bb52137db3c719fa
Nirajsinghns1008 <nirajsinghns1008@gmail.com>
2025-09-17 18:43:17 +0530

    hcf or gcd find

--- codechef.py
+++ codechef.py
@@ -200,7 +200,38 @@ class Solution:
         if num==count:
             return True
         else:
-            return False                 
+            return False    
+
+
+
+        """
+        You are given two integers n1 and n2. You need find the Greatest Common Divisor (GCD) of the two given numbers. Return the GCD of the two numbers.
+
+
+
+The Greatest Common Divisor (GCD) of two integers is the largest positive integer that divides both of the integers.
+
+
+Examples:
+Input: n1 = 4, n2 = 6
+
+Output: 2
+
+Explanation: Divisors of n1 = 1, 2, 4, Divisors of n2 = 1, 2, 3, 6
+
+Greatest Common divisor = 2.
+"""
+class Solution:
+    def GCD(self, n1, n2):
+        if n2 > n1:
+            mn=n1
+        else:
+            mn=n2
+        for i in range(1,mn+1):
+            if n1%i==0 and n2%i==0:
+                GCD = i
+        
+        return GCD                        
 
             
                  
